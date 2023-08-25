be7ef763d5797853f71e2b6c94493013aa8fdae6
C-W-Z <chenweizhang3021@gmail.com>
2023-08-25 10:31:31 +0800

    + Ray mode in CheckBox

--- Assets/Scripts/CheckBox.cs
+++ Assets/Scripts/CheckBox.cs
@@ -5,7 +5,8 @@ public class CheckBox : MonoBehaviour
     private enum Type
     {
         Rectangle,
-        Circle
+        Circle,
+        Ray
     }
 
     [SerializeField] private Transform tf;
@@ -13,6 +14,7 @@ public class CheckBox : MonoBehaviour
     [SerializeField] private Color _color = Color.red;
     [SerializeField] private float width, height;
     [SerializeField] private float radius;
+    [SerializeField] private Vector2 direction;
     // [SerializeField] private bool useTrigger2D = false;
     // [SerializeField] private string layerName;
     // private bool triggerEnter = false;
@@ -22,19 +24,46 @@ public class CheckBox : MonoBehaviour
         // if (useTrigger2D)
         //     return;
         Gizmos.color = _color;
-        if (type == Type.Circle)
-            Gizmos.DrawWireSphere(tf.position, radius);
-        else
-            Gizmos.DrawWireCube(tf.position, new Vector3(width, height, 0));
+        switch (type)
+        {
+            case Type.Rectangle:
+                Gizmos.DrawWireCube(tf.position, new Vector3(width, height, 0));
+                break;
+            case Type.Circle:
+                Gizmos.DrawWireSphere(tf.position, radius);
+                break;
+            case Type.Ray:
+                Gizmos.DrawRay(tf.position, direction);
+                break;
+        }
     }
 
     public bool Detect(LayerMask layer)
     {
         // if (useTrigger2D)
         //     return triggerEnter;
-        if (type == Type.Circle)
-            return Physics2D.OverlapCircle(tf.position, radius, layer);
-        return Physics2D.OverlapBox(tf.position, new Vector2(width, height), 0, layer);
+        switch (type)
+        {
+            case Type.Rectangle:
+                return Physics2D.OverlapBox(tf.position, new Vector2(width, height), 0, layer);
+            case Type.Circle:
+                return Physics2D.OverlapCircle(tf.position, radius, layer);
+            case Type.Ray:
+                return (bool)Physics2D.Raycast(tf.position, direction);
+            default:
+                return false;
+        }
+    }
+
+    public Vector2 GetHitPoint(Vector2 defaultPos)
+    {
+        if (type != Type.Ray)
+            return defaultPos;
+        RaycastHit2D hit = Physics2D.Raycast(tf.position, direction);
+        if (hit.collider != null)
+            return hit.point;
+        Debug.Log("no hit");
+        return defaultPos;
     }
 
     // void OnTriggerEnter2D(Collider2D collision)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -153,6 +153,8 @@ ledge_grabbing:
     [SerializeField] private CheckBox wallCheck;
     [SerializeField] private CheckBox ledgeCheck;
     [SerializeField] private CheckBox ledgeCheckTop;
+    [SerializeField] private CheckBox ledgeRayFront;
+    [SerializeField] private CheckBox ledgeRayDown;
     public bool onGround { get; private set; }
     private bool wallDetected;
     private bool ledgeDetected;
@@ -279,8 +281,6 @@ ledge_grabbing:
     [Header("Ledge Info")]
     [SerializeField] private Vector2 offsetBefore = new(0.14f, 0.22f);
     [SerializeField] private Vector2 offsetAfter = new(0.35f, 0.4f);
-    [SerializeField] private Vector2 rayRightOffset = new(0f, -0.18f);
-    [SerializeField] private Vector2 rayDownOffset = new(0.27f, 0f);
     private Vector2 ledgeClimbPosBefore;
     private Vector2 ledgeClimbPosAfter;
     private bool canGrabLedge = true;
@@ -291,16 +291,8 @@ ledge_grabbing:
     {
         // get corner position
         Vector2 cornerPos = tf.position;
-        RaycastHit2D hit;
-        hit = Physics2D.Raycast((Vector2)tf.position + rayRightOffset, isFaceRight ? Vector2.right : Vector2.left);
-        if (hit.collider != null)
-            cornerPos.x = hit.point.x;
-        else  Debug.Log("no front hit");
-        hit = Physics2D.Raycast((Vector2)tf.position + rayDownOffset * (isFaceRight ? 1 : -1), Vector2.down);
-        if (hit.collider != null)
-            cornerPos.y = hit.point.y;
-        else Debug.Log("no down hit");
-
+        cornerPos.x = ledgeRayFront.GetHitPoint(cornerPos).x;
+        cornerPos.y = ledgeRayDown.GetHitPoint(cornerPos).y;
         // set ledge grab and climb position
         ledgeClimbPosBefore = cornerPos + new Vector2(offsetBefore.x * (isFaceRight ? 1 : -1), offsetBefore.y);
         ledgeClimbPosAfter = cornerPos + new Vector2(offsetAfter.x * (isFaceRight ? 1 : -1), offsetAfter.y);
@@ -326,17 +318,5 @@ ledge_grabbing:
         canGrabLedge = true;
     }
 
-    private void DrawRayForLedgePos()
-    {
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawLine(tf.position + (Vector3)rayRightOffset, tf.position + (Vector3)rayRightOffset + new Vector3(0.5f * (isFaceRight ? 1 : -1), 0, 0));
-        Gizmos.DrawLine(tf.position + (Vector3)rayDownOffset * (isFaceRight ? 1 : -1), tf.position + (Vector3)rayDownOffset * (isFaceRight ? 1 : -1) + new Vector3(0, -0.5f, 0));
-    }
-
 #endregion
-
-    void OnDrawGizmos()
-    {
-        DrawRayForLedgePos();
-    }
 }
